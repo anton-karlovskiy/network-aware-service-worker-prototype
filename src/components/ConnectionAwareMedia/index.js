814f231701bb6a102239e1db4d9f09b783af5106
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-08-10 02:49:33 +0300

    implemented custom cache strategy which I think is more proper

--- src/components/ConnectionAwareMedia/index.js
+++ src/components/ConnectionAwareMedia/index.js
@@ -24,20 +24,21 @@ const ConnectionAwareMedia = () => {
 
   console.log('[ConnectionAwareMedia] effectiveConnectionType => ', effectiveConnectionType);
   let media;
-  // TODO: just use image types since other type response cannot be handled in image markup (e.g video reponse in image src -> does not make sense)
   switch (effectiveConnectionType) {
     case 'slow-2g':
-    case '2g':
-      media = <img className='responsive' src='https://cdn.glitch.com/8d7fb7f0-a9be-4a8c-96c7-8af286af487e%2Fmin-res.jpg?v=1562842586912' alt='high resolution' />;
+      media = <img className='responsive' src='https://cdn.glitch.com/8d7fb7f0-a9be-4a8c-96c7-8af286af487e%2Fmin-res.jpg?v=1562842586912' alt='low resolution' />;
       break;
-    case '3g':
+    case '2g':
       media = <img className='responsive' src='https://cdn.glitch.com/8d7fb7f0-a9be-4a8c-96c7-8af286af487e%2Fmedium-res.jpg?v=1562842587169' alt='medium resolution' />;
       break;
-    case '4g':
+    case '3g':
       media = <img className='responsive' src='https://cdn.glitch.com/8d7fb7f0-a9be-4a8c-96c7-8af286af487e%2Fmax-res.jpg?v=1562842587982' alt='high resolution' />;
       break;
+    case '4g':
+      media = <video className='responsive' src='https://cdn.glitch.com/8d7fb7f0-a9be-4a8c-96c7-8af286af487e%2F4g-video.mp4?v=1562842601068' controls />;
+      break;
     default:
-      media = <img className='responsive' src='https://cdn.glitch.com/8d7fb7f0-a9be-4a8c-96c7-8af286af487e%2Fmax-res.jpg?v=1562842587982' alt='high resolution' />;
+      media = <video className='responsive' src='https://cdn.glitch.com/8d7fb7f0-a9be-4a8c-96c7-8af286af487e%2F4g-video.mp4?v=1562842601068' controls />;
       break;
   }
 
