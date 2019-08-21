3fb5ac6b0e6f8a3b762244f14c6e202bfd042e12
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-08-22 00:42:57 +0300

    fixed resource switch block bug

--- src/components/ConnectionAwareMedia/index.js
+++ src/components/ConnectionAwareMedia/index.js
@@ -24,10 +24,10 @@ const ConnectionAwareMedia = () => {
 
   console.log('[ConnectionAwareMedia] effectiveConnectionType => ', effectiveConnectionType);
   let media;
-  // TODO: just use image types since other type response cannot be handled in image markup (e.g video reponse in image src -> does not make sense)
   switch (effectiveConnectionType) {
     case 'slow-2g':
-      media = <img className='responsive' src='https://cdn.glitch.com/8d7fb7f0-a9be-4a8c-96c7-8af286af487e%2Fmin-res.jpg?v=1562842586912' alt='high resolution' />;
+      media = <img className='responsive' src='https://cdn.glitch.com/8d7fb7f0-a9be-4a8c-96c7-8af286af487e%2Fmin-res.jpg?v=1562842586912' alt='low resolution' />;
+      break;
     case '2g':
       media = <img className='responsive' src='https://cdn.glitch.com/8d7fb7f0-a9be-4a8c-96c7-8af286af487e%2Fmedium-res.jpg?v=1562842587169' alt='medium resolution' />;
       break;
